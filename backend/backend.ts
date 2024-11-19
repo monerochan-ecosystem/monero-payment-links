14d0a81f8c9ec821fd2518a3cfcf1550526828da
Spirobel <spirobel@protonmail.com>
2024-11-19 20:37:27 +0800

    Initial commit (via bun create)

---
+++ backend/backend.ts
@@ -0,0 +1,20 @@
+import { url, head, commonHead, cssReset } from "@spirobel/mininext";
+head((mini) => mini.html`<title>hello hello</title>${commonHead}${cssReset}`);
+url.set([
+  [
+    "/",
+    (mini) => {
+      return mini.html`<h1>Hello world</h1>`;
+    },
+  ],
+  [
+    "/bye",
+    (mini) =>
+      mini.html`<h1>Goodbye world</h1>${mini.head(
+        mini.html`<title>bye bye</title>${commonHead}`
+      )}`,
+  ],
+]);
+url.set("/apple", (mini) => mini.html`<h1>hello pineapple </h1>`);
+
+export default url.install;
