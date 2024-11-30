584c6c9c3ab3c58d3d727da2793e7cfa34d3d3c0
Spirobel <spirobel@protonmail.com>
2024-11-30 12:00:12 +0800

    Page title --- might be too long? add favicon?

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,7 +1,7 @@
 import { url, head, commonHead, cssReset } from "@spirobel/mininext";
 import { LoginCheck } from "./users/loginLogout";
 import { dashBoardIndex } from "./dashboard";
-head((mini) => mini.html`<title>hello hello</title>${commonHead}`);
+head((mini) => mini.html`<title>Monero Payment Links</title>${commonHead}`);
 url.set("/", LoginCheck(dashBoardIndex));
 
 export default url.install;
