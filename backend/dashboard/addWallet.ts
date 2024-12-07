3a7f6421efa57db35b46c4f12eedfd0088988733
Spirobel <spirobel@protonmail.com>
2024-12-08 01:32:29 +0800

    save the wallet to the wallets table

--- backend/dashboard/addWallet.ts
+++ backend/dashboard/addWallet.ts
@@ -1,11 +1,18 @@
 import type { Mini } from "@spirobel/mininext";
 import type { Loggedin } from "../users/loginLogout";
-import { WalletSchema } from "../../db/schema";
+import { wallets, WalletSchema } from "../../db/schema";
+import { db } from "../../db/db";
 
 export function addWalletEndpoint(mini: Mini<Loggedin>) {
   console.log(mini.form.formJson);
   const result = WalletSchema.safeParse(mini.form.formJson);
   if (result.data) {
+    const insertedRow = db
+      .insert(wallets)
+      .values(result.data)
+      .returning()
+      .get();
+    return mini.json`${insertedRow}`;
   }
   return mini.json`${result}`;
 }
