f5f55e85b23265744bf093825f6294f465d37dcc
Spirobel <spirobel@protonmail.com>
2026-04-23 02:07:12 +0800

    set wallet_slot when making 002 tool link

--- dashboard/frontend /wallets/wallets_list.ts
+++ dashboard/frontend /wallets/wallets_list.ts
@@ -2,8 +2,8 @@ import { html, flatten, type MiniHtmlString } from "@spirobel/mininext";
 import { make002ToolLink } from "@spirobel/monero-wallet-api";
 import { router } from "../dashboard_router";
 
-function makeWalletCreationLink() {
-  const makeWalletLink = make002ToolLink(0);
+function makeWalletCreationLink(wallet_slot: number = 0) {
+  const makeWalletLink = make002ToolLink(wallet_slot);
   return router.link("/wallets/new/") + makeWalletLink;
 }
 export function walletGrid() {
